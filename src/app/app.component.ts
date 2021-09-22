9fe5fe6db5997d42b76def43102685a7b9755113
johnson_lin0930 <>
2021-09-22 18:11:57 +0800

    fix: little fix

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,8 +5,7 @@ import { stationList } from './station-list.const';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   /**
